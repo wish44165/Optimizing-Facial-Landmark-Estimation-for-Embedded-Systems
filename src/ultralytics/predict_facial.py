da1d0065a91abb527a1075da7a4790019efdeb3e
yuhsi chen <38905980+wish44165@users.noreply.github.com>
2024-02-17 00:11:39 +1100

    Delete src/ultralytics/predict_facial.py

--- src/ultralytics/predict_facial.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-model = YOLO('./runs/facial/train/weights/best.pt')  # load a custom model
-
-# Predict with the model
-results = model('./data/images/val/', save=True, project="./runs/facial")
