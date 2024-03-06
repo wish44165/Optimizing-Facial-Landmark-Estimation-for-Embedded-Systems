8ed688687b4045f6d8782ed32f1095404dffef64
yuhsi chen <38905980+wish44165@users.noreply.github.com>
2024-03-06 15:31:13 +1100

    Add files via upload

--- qualification/ultralytics/predict.py
+++ qualification/ultralytics/predict.py
@@ -5,7 +5,7 @@ def parse_opt(known=False):
     parser = argparse.ArgumentParser()
     parser.add_argument('--weight', type=str, default='./runs/facial/train/weights/best.pt', help='trained model')
     parser.add_argument('--save_path', type=str, default='./runs/facial', help='Save path')
-    parser.add_argument('--predict_folder', type=str, default='../preprocess/data_v2/images/val/', help='Predict folder')
+    parser.add_argument('--predict_folder', type=str, default='/home/wish/pro/ICME2024/datasets/ivslab_facial_test_private_qualification', help='Predict folder')
     return parser.parse_known_args()[0] if known else parser.parse_args()
 
 opt = parse_opt()
@@ -14,4 +14,4 @@ opt = parse_opt()
 model = YOLO(opt.weight)  # load a custom model
 
 # Predict with the model
-results = model(opt.predict_folder, save=True, save_txt=True, project=opt.save_path)
+results = model(opt.predict_folder, save_txt=True, project=opt.save_path)
--- qualification/ultralytics/train.py
+++ qualification/ultralytics/train.py
@@ -8,13 +8,23 @@ os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
 
 def parse_opt(known=False):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_name', type=str, default='yolov8n-pose.pt', help='model name')
-    parser.add_argument('--yaml_path', type=str, default='./facial.yaml', help='The yaml path')
+    parser.add_argument('--model_name', type=str, default='/home/wish/pro/ICME2024/src/iAutolabeling_conf_0.2/runs/facial/train5/weights/best.pt', help='model name')
+    parser.add_argument('--yaml_path', type=str, default='./facial_v4.yaml', help='The yaml path')
     parser.add_argument('--n_epoch', type=int, default=300, help='Total number of training epochs.')
     parser.add_argument('--n_patience', type=int, default=100, help='Number of epochs to wait without improvement in validation metrics before early stopping the training.')
-    parser.add_argument('--bs', type=int, default=48, help='Batch size')
+    parser.add_argument('--bs', type=int, default=40, help='Batch size')
     parser.add_argument('--imgsz', type=int, default=640, help='Image size')
+
+    parser.add_argument('--single_cls', type=bool, default=True, help='single class or not')
     parser.add_argument('--pose', type=float, default=12.0, help='Weight of the pose loss in models trained for pose estimation, influencing the emphasis on accurately predicting pose keypoints.')
+    parser.add_argument('--degrees', type=float, default=10, help='')
+    parser.add_argument('--shear', type=float, default=0.2, help='')
+    parser.add_argument('--mosaic', type=float, default=0.0, help='')
+    parser.add_argument('--mixup', type=float, default=0.2, help='')
+    parser.add_argument('--copy_paste', type=float, default=0.2, help='')
+    parser.add_argument('--erasing', type=float, default=0.0, help='')
+
+
     parser.add_argument('--n_worker', type=int, default=8, help='Number of workers')
     parser.add_argument('--save_path', type=str, default='./runs/facial', help='Save path')
     return parser.parse_known_args()[0] if known else parser.parse_args()
@@ -26,4 +36,6 @@ model = YOLO(opt.model_name)  # load a pretrained model (recommended for trainin
 
 if __name__ == '__main__':
     # Train the model
-    model.train(data=opt.yaml_path, epochs=opt.n_epoch, patience=opt.n_patience, batch=opt.bs, imgsz=opt.imgsz, device=0, workers=opt.n_worker, project=opt.save_path, pose=opt.pose)
+    model.train(data=opt.yaml_path, epochs=opt.n_epoch, patience=opt.n_patience, batch=opt.bs, imgsz=opt.imgsz, device=0, workers=opt.n_worker, 
+                project=opt.save_path, single_cls=opt.single_cls, pose=opt.pose, degrees=opt.degrees, shear=opt.shear, mosaic=opt.mosaic,
+                mixup=opt.mixup, copy_paste=opt.copy_paste, erasing=opt.erasing)
