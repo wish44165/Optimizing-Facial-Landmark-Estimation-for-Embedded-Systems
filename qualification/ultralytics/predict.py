c6172f903a8f76e35892f2054e1ef244b2cbca7e
yuhsi chen <38905980+wish44165@users.noreply.github.com>
2024-03-07 22:21:40 +1100

    add conf

--- qualification/ultralytics/predict.py
+++ qualification/ultralytics/predict.py
@@ -3,9 +3,10 @@ from ultralytics import YOLO
 
 def parse_opt(known=False):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weight', type=str, default='./runs/facial/train/weights/best.pt', help='trained model')
+    parser.add_argument('--weight', type=str, default='./runs/facial/train2/weights/best.pt', help='trained model')
     parser.add_argument('--save_path', type=str, default='./runs/facial', help='Save path')
     parser.add_argument('--predict_folder', type=str, default='/home/wish/pro/ICME2024/datasets/ivslab_facial_test_private_qualification', help='Predict folder')
+    parser.add_argument('--conf', type=float, default=0.65, help='')
     return parser.parse_known_args()[0] if known else parser.parse_args()
 
 opt = parse_opt()
@@ -14,4 +15,4 @@ opt = parse_opt()
 model = YOLO(opt.weight)  # load a custom model
 
 # Predict with the model
-results = model(opt.predict_folder, save_txt=True, project=opt.save_path, conf=0.65)
+results = model(opt.predict_folder, save=True, save_txt=True, project=opt.save_path, conf=opt.conf)
