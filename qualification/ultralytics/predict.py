7d3c5979aa0456ca060e19f2741f55e0c583731a
yuhsi chen <38905980+wish44165@users.noreply.github.com>
2024-03-09 13:26:03 +1100

    add imgsz

--- qualification/ultralytics/predict.py
+++ qualification/ultralytics/predict.py
@@ -6,6 +6,7 @@ def parse_opt(known=False):
     parser.add_argument('--weight', type=str, default='./runs/facial/train2/weights/best.pt', help='trained model')
     parser.add_argument('--save_path', type=str, default='./runs/facial', help='Save path')
     parser.add_argument('--predict_folder', type=str, default='/home/wish/pro/ICME2024/datasets/ivslab_facial_test_private_qualification', help='Predict folder')
+    parser.add_argument('--imgsz', type=int, default=640, help='Image size')
     parser.add_argument('--conf', type=float, default=0.65, help='')
     return parser.parse_known_args()[0] if known else parser.parse_args()
 
@@ -15,4 +16,4 @@ opt = parse_opt()
 model = YOLO(opt.weight)  # load a custom model
 
 # Predict with the model
-results = model(opt.predict_folder, save=True, save_txt=True, project=opt.save_path, conf=opt.conf)
+results = model(opt.predict_folder, save=True, save_txt=True, project=opt.save_path, imgsz=opt.imgsz, conf=opt.conf)
